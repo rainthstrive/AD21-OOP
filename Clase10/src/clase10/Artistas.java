81d252248bc3e878764204836da5ae6dc52892f9
Arturo Garcia <rainthstrive@gmail.com>
2021-09-10 08:52:52 -0500

    Clase 10 terminada

--- Clase10/src/clase10/Artistas.java
+++ Clase10/src/clase10/Artistas.java
@@ -5,8 +5,8 @@ public class Artistas{
     private Canciones[] canciones = new Canciones[5];
     private int index = 0;
 
-    Artistas(String nombre, String tipo){
-        setNombreArtista(nombre);
+    Artistas(String nombreArtista){
+        setNombreArtista(nombreArtista);
     }
 
     public String getNombreArtista(){
@@ -24,14 +24,38 @@ public class Artistas{
         {
             canciones[index] = cancion;
             aumentarIndex();
+            System.out.println("¡Canción agregada a " + getNombreArtista() + "!");
         }
         else
-            System.out.println("L�mite de canciones alcanzado");
+            System.out.println("Límite de canciones alcanzado");
+    }
+    
+    public void agregarCancion(Canciones[] cancion){
+        for(Canciones el: cancion) {
+        	if(index < canciones.length)
+            {
+                canciones[index] = el;
+                aumentarIndex();
+            }
+        	else
+                System.out.println("Límite de canciones alcanzado");
+        }
     }
 
-    // TODO: Definir m�todo para agregar multiples canciones
-
     public void listarCanciones(){
-        // TODO: Definir instrucciones para listar canciones
+    	if(canciones[0]!=null) {
+    		System.out.println("__________________________");
+	    	System.out.println("****Canciones de "+ getNombreArtista() +"****");
+	        for(Canciones el: canciones) {
+	        	if(el != null)
+	        		System.out.println(el.getNombreCancion() + " - " + el.getTipoMusica());
+	        	else
+	        		break;
+	        }
+	        System.out.println("****Fin de la lista****");
+	        System.out.println("__________________________");
+    	} else
+    		System.out.println(getNombreArtista() + " no tiene canciones 😴");
+    	
     }
 }
--- Clase10/src/clase10/CatalogoMusical.java
+++ Clase10/src/clase10/CatalogoMusical.java
@@ -2,45 +2,133 @@ package clase10;
 
 import java.util.Scanner;
 
-public class CatalogoMusical {
+public class CatalogoMusical extends Thread{
     private static Scanner scanner = new Scanner(System.in);
     private boolean loopMenu = true;
     private int opcion;
-    // TODO: Definir variable para artistas
+    private Artistas[] artistas = new Artistas[3];
+    private int index = 0;
 
     CatalogoMusical(){
+    	datosAutomaticos();
 		empezar();
 	};
 	
 	private void empezar() {
-		// Men�
+		// Menú
 		do {
-			System.out.println("Bienvenido �Qu� acci�n deseas realizar?");
+			System.out.println("Bienvenido ¿Qué acción deseas realizar?");
 			System.out.println("Agregar Artista: 1.");
-            System.out.println("Agregar Canci�n: 2.");
-            System.out.println("Listar canciones de artista: 3.");
-            System.out.println("Salir: 4");
+			System.out.println("Listar Artistas: 2.");
+            System.out.println("Agregar Canción: 3.");
+            System.out.println("Listar canciones de artista: 4.");
+            System.out.println("Salir: 99");
 			opcion = scanner.nextInt();
-			
+			scanner.nextLine();
 			switch(opcion) {
 				case 1: 
-					// TODO: Agregar Artista
+					agregarArtistas();
 					break;
-				case 2: 
-					// TODO: Agregar Canci�n a un Artista
+				case 2:
+					listarArtistas();
 					break;
-                case 3: 
-					// TODO: Listar las canciones de un artista
-					break;    
+				case 3: 
+					agregarCancion();
+					break;
                 case 4: 
+                	listarCancionesDeArtista();
+					break;    
+                case 99: 
 					loopMenu = false;
 					break;
 				default:
-					System.out.println("Opci�n no definida");
+					System.out.println("Opción no definida");
 					break;
 			}
 		} while(loopMenu);
         // Salida del programa
-		System.out.println("�Gracias por usar el cat�logo de artistas!");
+		System.out.println("¡Gracias por usar el catálogo de artistas!");
 	}
+	private void listarArtistas() {
+		if(artistas[0] != null)
+		{
+			System.out.println("__________________________");
+	    	System.out.println("**** Artistas Disponibles ****");
+			for(int i = 0; i < artistas.length; i++) {
+				if(artistas[i] != null)
+					System.out.println("[" + i + "] - " + artistas[i].getNombreArtista());
+				else
+					break;
+			}
+	    	System.out.println("**** Fin de la lista ****");
+			System.out.println("__________________________");
+		}
+		else
+			System.out.println("No hay artistas 😫");
+	}
+	private void listarCancionesDeArtista() {
+		System.out.println("Ingrese el índice de artista a buscar:");
+		int indice = scanner.nextInt();
+		scanner.nextLine();
+		artistas[indice].listarCanciones();
+	}
+	private void agregarCancion(){
+		System.out.println("Ingrese el índice de artista a buscar:");
+		int indice = scanner.nextInt();
+		scanner.nextLine();
+		System.out.println("Ingrese nombre de canción para " + artistas[indice].getNombreArtista());
+		String nombre = scanner.nextLine();
+		System.out.println("Ingrese el tipo de canción que es " + nombre);
+		String tipo = scanner.nextLine();
+		System.out.print("Agregando canción.");
+		for(int i = 0; i < 8; i++) {
+			try {
+				sleep(500);
+			} catch (InterruptedException e) {
+				System.out.println("¡Ocurrió un error!");
+			}
+			System.out.print(".");
+		}
+			try {
+				System.out.print("\n");
+				artistas[indice].agregarCancion(new Canciones(nombre, tipo));
+			}
+			catch(Exception e) {
+				// Qué indice quiso acceder?
+				// Guardar ese indica
+				// Tomar accion sobre ese indice
+				// Ej. Buscar el primer indice disponible
+				// Realizar un for, buscando el primer elemento nulo del arreglo
+				// Guardar nueva cancion en ese indice
+				System.out.println("Ocurrió un error al agregar canción: " + e);
+			}
+	}
+	private void agregarArtistas() {
+		if(index < artistas.length)
+        {
+			String nombre;
+			System.out.println("Ingrese nombre de artista");
+			nombre = scanner.nextLine();
+			this.artistas[index] = new Artistas(nombre);
+            aumentarIndex();
+        }
+    	else
+            System.out.println("Límite de canciones alcanzado");
+	}
+	private void datosAutomaticos() {
+		this.artistas[index] = new Artistas("Seal");
+		Canciones[] canciones = {
+				new Canciones("Fly Like an Eagle", "Funk"),
+				new Canciones("Stand by Me", "Pop"),
+				new Canciones("Kiss From a Rose", "R&B"),
+				new Canciones("Crazy", "Soul")
+		};
+		this.artistas[index].agregarCancion(canciones);
+        aumentarIndex();
+        this.artistas[index] = new Artistas("Michael Jackson");
+        aumentarIndex();
+	}
+	private void aumentarIndex(){
+        index++;
+    }
 }
